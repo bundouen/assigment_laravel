9f048581f62b22d15480095a0f7cc01e8b3b0a45
Bundouen <longbundouen@gmail.com>
2022-05-28 12:08:09 +0700

    updated cart item

--- resources/views/frontend/cart.blade.php
+++ resources/views/frontend/cart.blade.php
@@ -5,14 +5,29 @@
 @section('content')
 <div class="container my-2">
     <div class="card shadow rounded product_data">
+        <div class="card-header">
+            @if ($cartItem->count()==null)
+                <h2 class="text-center">No Cart Item</h2>
+            @else
+                <h2 class="text-center">All Cart Item of {{ Auth::user()->name }}</h2>
+            @endif
+
+        </div>
         <div class="card-body">
+            @php
+                $total=0;
+            @endphp
             @foreach ($cartItem as $item )
             <div class="row product_data">
                 <div class="col-md-2">
                     <img src="{{ asset('asset/uploads/product/'.$item->products->image) }}" class="w-50" alt="">
                 </div>
-                <div class="col-md-5">
-                    <h3>{{ $item->products->name }}</h3>
+                <div class="col-md-4">
+                    <h6>{{ $item->products->name }}</h6>
+                   
+                </div>
+                <div class="col-md-2">
+                    <h6>$ {{ $item->products->selling_price }}</h6>
                 </div>
                 <div class="col-md-3">
                     <input type="hidden" class="prod_id" value="{{ $item->prod_id }}">
@@ -23,15 +38,22 @@
                         <button class="input-group-text increment-btn">+</button>
                     </div>
                 </div>
-                <div class="col-md-2">
+                <div class="col-md-1">
                     <button class="btn btn-danger delete-cart-item"><i class="fa fa-trash"></i></button>
                 </div>
-                
+
             </div>
             <hr>
+            @php
+                $total+=$item->products->selling_price * $item->prod_qty
+            @endphp
             @endforeach
         </div>
-
+        <div class="card-footer">
+            <h3>Total Price=$ {{ $total }}
+                <button class="btn btn-primary btn-checkout float-end" style="font-size: 22px">Check Out</button>
+            </h3>
+        </div>
     </div>
 </div>
 @endsection
